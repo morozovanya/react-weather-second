1e99164f140c603493f8ccad3a637be9bc1f7af2
Anna <Anytamorozov@gmail.com>
2022-12-12 16:35:43 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="https://www.facebook.com/profile.php?id=100008369254419">Anna Morozova</a> and is {" "}
+        <a href="https://github.com/morozovanya/react-weather-second" target="_blank" rel="noreferrer">
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
