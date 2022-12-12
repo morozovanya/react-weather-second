4d22a8c17c2898f24c234484340b3b7146819101
Anna <Anytamorozov@gmail.com>
2022-12-12 21:42:01 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
       <h1>Weather App</h1>
       <footer>
         This project was coded by <a href="https://www.facebook.com/profile.php?id=100008369254419">Anna Morozova</a> and is {" "}
         <a href="https://github.com/morozovanya/react-weather-second" target="_blank" rel="noreferrer">
           open-sourced
         </a>
-      </footer>
+        </footer>
+        </div>
     </div>
   );
 }
